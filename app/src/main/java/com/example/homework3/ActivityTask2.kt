74fefe61a5ee754b2ea504faada8462df659b58a
aazmundz@gmail.com <aazmundz@gmail.com>
2022-05-28 18:34:04 +0300

    work to check

--- app/src/main/java/com/example/homework3/ActivityTask2.kt
+++ app/src/main/java/com/example/homework3/ActivityTask2.kt
@@ -1,5 +1,6 @@
 package com.example.homework3
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
@@ -8,6 +9,10 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class ActivityTask2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +28,28 @@ class ActivityTask2 : AppCompatActivity() {
                 Glide.with(applicationContext)
                     .load(url)
                     .placeholder(imageView.drawable)
-                    .error(Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT).show())
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                    })
                     .into(imageView)
 
                 return@OnKeyListener true
